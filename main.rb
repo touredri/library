e02dd670942ec3294081b21d51adaf23b9ecca19
touredri <dcompte223@gmail.com>
2023-06-22 12:55:25 +0000

    fix linter

--- main.rb
+++ main.rb
@@ -13,12 +13,12 @@ capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 capitalized_trimmed_person.correct_name
 
-terminal = Classroom.new("class1")
-std1 = Student.new(22, "elv1")
+terminal = Classroom.new('class1')
+std1 = Student.new(22, 'elv1')
 terminal.add_student(std1)
 puts std1.classroom.label
 
-book1 = Book.new("Legendary", "author1")
-rental = Rental.new("Today", book1, person)
+book1 = Book.new('Legendary', 'author1')
+rental = Rental.new('Today', book1, person)
 puts book1.rentals[0].date
 puts person.rentals[0].book.author
