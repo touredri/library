cbf37094b6affac9eacaf510fc807c46054e2b44
touredri <dcompte223@gmail.com>
2023-06-23 21:04:37 +0100

    fixed linters

--- app.rb
+++ app.rb
@@ -29,76 +29,76 @@ class App
 
   # create a new person
   def self.create_person
-    print "Do you want to create a teacher(1) or student(2)? [Input the number] : "
+    print 'Do you want to create a teacher(1) or student(2)? [Input the number] : '
     nb = gets.chomp.to_i
     unless [1, 2].include?(nb)
-      puts "Invalid : You must enter 1 ou 2"
+      puts 'Invalid : You must enter 1 ou 2'
       self.create_person()
     end
     case nb
     when 1
-      print "Name: "
+      print 'Name: '
       name = gets.chomp
-      print "Age: "
+      print 'Age: '
       age = gets.chomp
-      print "Specialisation: "
+      print 'Specialisation: '
       spc = gets.chomp
       new_person = Teacher.new(spc, age, name)
-      puts "Teacher created successfully"
+      puts 'Teacher created successfully'
       App.list_persons.push(new_person)
     when  2
-      print "Name: "
+      print 'Name: '
       name = gets.chomp
-      print "Age: "
+      print 'Age: '
       age = gets.chomp.to_i
-      print("Has parent permission? (Y/N: ")
+      print('Has parent permission? (Y/N: ')
       per = gets.chomp
       new_person = Student.new(age ,name)
       new_person.parent_permission = per
-      puts "Student created successfully"
+      puts 'Student created successfully'
       App.list_persons.push(new_person)
     end
   end
 
   # Create a new  book
   def self.create_book
-    print "Title: "
+    print 'Title: '
     tl = gets.chomp
-    print "Author: "
+    print 'Author: '
     au = gets.chomp
     new_book = Book.new(tl, au)
     App.list_books.push(new_book)
-    puts "Book created and added to book list successfully"
+    puts 'Book created and added to book list successfully'
   end
 
   # create a new rental
   def self.create_rental
-    p "Select a book from the following list by number: "
-    App.list_books.each do |book, index=0|
-      p "#{index + 1}) Title: #{book.title} by Author: #{book.author}"
+    puts 'Select a book from the following list by number: '
+    App.list_books.each do |book, index = 0|
+      puts "#{index + 1}) Title: #{book.title} by Author: #{book.author}"
     end
     book_index = gets.chomp.to_i - 1
     if book_index == 0 || book_index >= book.length || (begin Integer(book_index); false; rescue; true; end)
-      p "Please enter a book valid number"
+      puts 'Please enter a book valid number'
       self.create_rental
     end
     book = App.list_books[book_index]
-    p "Select a person from the following list by number: "
-    App.list_persons.each do |person, index=0|
+    puts 'Select a person from the following list by number: '
+    App.list_persons.each do |person, index = 0|
       print "#{index + 1}) "
       if person.respond_to?(:specialisation)
-        p "#{index +1} [Teacher] Name: #{person.name}  ID: #{person.id} Age: #{person.age} old is a #{person.specialisation}"
+        puts "#{index +1} [Teacher] Name: #{person.name}  ID: #{person.id} Age: #{person.age} old is a #{person.specialisation}"
       end
       if person.respond_to?(:parent_permission)
-        p "#{index +1} [Student] Name: #{person.name}  ID: #{person.id} Age: #{person.age} old has permissions: #{person.parent_permission} "
+        puts "#{index +1} [Student] Name: #{person.name}  ID: #{person.id} Age: #{person.age} old has permissions: #{person.parent_permission} "
       end
     end
     person_index = gets.chomp.to_i - 1
     person = App.list_persons[person_index]
-    print "Date: "
+    print 'Date: '
     date = gets.chomp
     book.add_rental(person, date)
-    puts "Rental created successfully"
+    puts 'Rental created successfully'
   end
   # list all rental for a given person
   def self.list_rental
--- main.rb
+++ main.rb
@@ -5,21 +5,21 @@ require_relative 'book'
 require_relative 'app'
 def main
   puts 'Please choose an option by entering a number'
-  puts "1 - List all Books"
-  puts "2 - List all Person"
-  puts "3 - Create a person"
-  puts "4 - Create a book"
-  puts "5 - Create a Rental"
-  puts "6 - List all Rentals for a given person"
-  puts "7 - Exit"
+  puts '1 - List all Books'
+  puts '2 - List all Person'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a Rental'
+  puts '6 - List all Rentals for a given person'
+  puts '7 - Exit'
   nbr = gets.chomp.to_i
   unless [1, 2, 3, 4, 5, 6, 7].include?(nbr)
-    puts "Invalid : You must choose one of the following options(number)"
+    puts 'Invalid : You must choose one of the following options(number)'
     main()
   end
   case nbr
   when 1
-    puts "this is the list of Books"
+    puts 'this is the list of Books'
     App.list_books.each do |book, i=0|
       p "#{i + 1}) Title: #{book.title} by Author: #{book.author}"
     end
@@ -28,10 +28,12 @@ def main
     App.list_persons.each do |person, i=0|
       print "#{i + 1}"
       if person.respond_to?(:specialisation)
-        puts "[Teacher] Name: #{person.name}  ID: #{person.id} Age: #{person.age} old is a #{person.specialisation}"
+        puts "[Teacher] Name: #{person.name}  ID: #{person.id}
+              Age: #{person.age} old is a #{person.specialisation}"
       end
       if person.respond_to?(:classroom)
-        puts "[Student] Name: #{person.name}  ID: #{person.id} Age: #{person.age} old has permissions: #{person.parent_permission} "
+        puts "[Student] Name: #{person.name}  ID: #{person.id}
+             Age: #{person.age} old has permissions: #{person.parent_permission} "
       end
     end
   when 3
@@ -45,10 +47,10 @@ def main
   when 7
     exit
   else
-    puts "Please enter a valid number"
+    puts 'Please enter a valid number'
   end
   main()
 end
 
-p "Welcome to School library App"
+p 'Welcome to School library App'
 main()
